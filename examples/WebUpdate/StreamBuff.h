dd04e8b637c89dfbe9f264febb64815f297a4da8
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-08-11 16:13:18 +0500

    example added

---
+++ examples/WebUpdate/StreamBuff.h
@@ -0,0 +1,25 @@
+#pragma once
+/*
+class StreamBuff : public Stream {
+public:
+  StreamBuff(char* b) {
+    buff = b;
+  }
+  int available() {
+    ;
+  }
+  int read() {
+    ;
+  }
+  int peek() {
+    ;
+  }
+  void flush() {
+    ;
+  }
+   onEmply
+private:
+  pos = 0;
+  char* buff
+};
+*/
